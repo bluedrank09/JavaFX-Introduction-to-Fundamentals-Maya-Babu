92fb8deca10e4fcdfa328dd58e6eaf0eecd73a90
Maya Babu <mayababu@Mayas-MacBook-Air-Jarvis.local>
2026-07-24 18:57:11 +1200

    feat: added implementation for the label to change when a button is pressed.

--- src/main/java/nz/ac/auckland/se206/controllers/CounterController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CounterController.java
@@ -1,8 +1,11 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import nz.ac.auckland.se206.App;
 
 public class CounterController {
     private int counter = 0;
@@ -23,7 +26,16 @@ public class CounterController {
     private Label numberLabel;
 
     @FXML
-    public void switchToMusic(){
+    public void switchToMusic(ActionEvent event){
+        Button button = (Button) event.getSource();
+        Scene sceneButtonIsIn = button.getScene();
+
+        try{
+            sceneButtonIsIn.setRoot(App.loadFXML("musicplayer"));
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
 
     }
 
