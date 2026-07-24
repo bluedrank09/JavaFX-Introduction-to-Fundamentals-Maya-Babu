9bafcc5eb690ca1976040c254efffa901a9b78c0
Maya Babu <mayababu@Mayas-MacBook-Air-Jarvis.local>
2026-07-24 17:42:49 +1200

    feat: created private fields in CounterController class for buttons in SceneBuilder scene

--- src/main/java/nz/ac/auckland/se206/controllers/CounterController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CounterController.java
@@ -1,5 +1,23 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class CounterController {
+    private int counter = 0;
+    
+    @FXML
+    private Button incrementButton;
+
+    @FXML
+    private Button switchToMusicButton;
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private Button decrementButton;
+
+    
     
 }
