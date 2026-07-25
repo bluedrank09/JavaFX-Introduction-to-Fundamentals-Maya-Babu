c11e3caf035a54d4b53c0c84de52bae92277b9e7
Maya Babu <mayababu@Mayas-MacBook-Air-Jarvis.local>
2026-07-25 18:34:21 +1200

    fix: fixed file and class name.

---
+++ src/main/java/nz/ac/auckland/se206/controllers/MusicPlayerController.java
@@ -0,0 +1,12 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+
+public class MusicPlayerController {
+
+    @FXML private void initialize(){
+        System.out.println("*** Initialising music player ***" + this);
+
+    }
+    
+}
